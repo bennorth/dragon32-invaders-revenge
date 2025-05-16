431172b287f9f7f6a0537a7c55a36d7dcf1b165e
Ben North <ben@redfrontdoor.org>
2025-05-16 09:56:52 +0100

    Write WAVs to own directory

--- New-sounds/invrevsounds.py
+++ New-sounds/invrevsounds.py
@@ -163,6 +163,12 @@ class SoundEffect:
 
 ############################################################################
 
+def out_fname(basestem):
+    return f"synthesised-output/{basestem}.wav"
+
+
+############################################################################
+
 if __name__ == "__main__":
     # Normal/special defender hit tails
     chirp_info = []
@@ -340,12 +346,33 @@ if __name__ == "__main__":
 
     end_of_game = SoundEffect([Chirp(0.063853e-3, 31.29e-6, 75)])
 
-    player_hit.write_wav("ph.wav", std_chirp_duration)
-    normal_defender_hit.write_wav("ndh.wav", std_chirp_duration)
-    special_defender_hit.write_wav("sdh.wav", std_chirp_duration)
-    player_shot.write_wav("psh.wav", std_chirp_duration)
-    defender_base_hit.write_wav("dbh.wav", std_chirp_duration)
-    end_of_game.write_wav("eog.wav", std_chirp_duration)
-    shot_hit.write_wav("sh.wav", std_chirp_duration)
+    player_hit.write_wav(
+        out_fname("player-hit"),
+        std_chirp_duration
+    )
+    normal_defender_hit.write_wav(
+        out_fname("normal-defender-hit"),
+        std_chirp_duration
+    )
+    special_defender_hit.write_wav(
+        out_fname("special-defender-hit"),
+        std_chirp_duration
+    )
+    player_shot.write_wav(
+        out_fname("player-shot"),
+        std_chirp_duration
+    )
+    defender_base_hit.write_wav(
+        out_fname("defender-base-hit"),
+        std_chirp_duration
+    )
+    end_of_game.write_wav(
+        out_fname("end-of-game"),
+        std_chirp_duration
+    )
+    shot_hit.write_wav(
+        out_fname("shot-hit"),
+        std_chirp_duration
+    )
 
     print("done")
