9e2434cddd1911896081e33a0851422fd40d80ff
Ben North <ben@redfrontdoor.org>
2025-05-25 20:28:06 +0100

    Add ability to emit coords as Loc_xNNN_yNNN

--- coords_from_addr.py
+++ coords_from_addr.py
@@ -2,4 +2,10 @@ import sys
 
 addr = int(sys.argv[1], 16);
 row, col = divmod(addr - 0x0600, 32)
-print(f"({col * 4}, {row})")
+
+fmt = "lbl" if len(sys.argv) > 2 and sys.argv[2] == "lbl" else "paren"
+
+if fmt == "lbl":
+    print(f"Loc_x{(col * 4):03d}_y{row:03d}")
+else:
+    print(f"({col * 4}, {row})")
