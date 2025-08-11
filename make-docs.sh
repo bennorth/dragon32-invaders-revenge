e485338ef67364f9a155439b4f17a9c47a549d88
Ben North <ben@redfrontdoor.org>
2025-08-11 08:50:35 +0100

    Add script to render write-up

---
+++ make-docs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p docs
+
+pandoc --css=styling.css \
+       -s \
+       -f markdown+smart \
+       --to=html5 \
+       write-up.md \
+       -o docs/index.html
+
+./make-html.sh
+
+cp -u \
+   docs-sources/styling.css \
+   docs-sources/cassette-inlay.jpg \
+   docs-sources/screenshot-1.png \
+   docs
+
+cp -u \
+   rendered-asm/out.html \
+   docs/disassembly.html
+
+cp -u \
+   rendered-asm/invaders-revenge.css \
+   docs/disassembly.css
